73781def49689ac4ec9b3706eaa310be4d5d76cb
Opeoluwa <olagokeopeolu1@gmail.com>
2022-07-17 23:39:08 +0100

    Handle navigations on index activity

--- app/src/main/java/com/example/bankapp/IndexActivity.java
+++ app/src/main/java/com/example/bankapp/IndexActivity.java
@@ -2,13 +2,33 @@ package com.example.bankapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class IndexActivity extends AppCompatActivity {
+
+    private Button loginBtn, signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
+
+        //Handle user action to switch to activities
+        switchActivities();
+    }
+
+    private void switchActivities() {
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        signupBtn = (Button) findViewById(R.id.signupBtn);
+
+        loginBtn.setOnClickListener(view -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+
+        signupBtn.setOnClickListener(view -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+        });
     }
 }
