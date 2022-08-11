eb4c72d8327a9aeef623968282265c603d9446a5
Opeoluwa <olagokeopeolu1@gmail.com>
2022-08-11 22:08:45 +0100

    Create user model

---
+++ app/src/main/java/com/example/bankapp/model/User.java
@@ -0,0 +1,93 @@
+package com.example.bankapp.model;
+
+public class User {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public User(String firstname, String lastname, String phone, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(int id, String firstname, String lastname, String phone, String email) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
